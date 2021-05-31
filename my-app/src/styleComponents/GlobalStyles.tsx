cb4f5017955a7edee25f653d411860d99611c207
Rinon <rinon.ten@onja.org>
2021-05-31 10:38:29 +0300

    Adds global styles

---
+++ my-app/src/styleComponents/GlobalStyles.tsx
@@ -0,0 +1,13 @@
+// globalStyles.js
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  main {
+        background-color: #f9f9f9;
+        font-family: Inter;
+        font-size: 16px;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal
+  }
+`
